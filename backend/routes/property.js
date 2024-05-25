e07515969c0919520aa74eed9516d2ef3ba709ba
Dharshan-devops <dharshans008@gmail.com>
2024-05-25 16:42:10 +0530

    added Seller propery endpoint

--- backend/routes/property.js
+++ backend/routes/property.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const Authorization = require('../middlewares/verifyAuth')
+const Property = require('../Models/propertyModel');
 
 // addProperty Endpoint
 const addProperty = require('../controllers/addProperty')
@@ -29,5 +30,28 @@ router.post("/filterProperty",filterProperties)
 const contactSeller=require('../controllers/contactSeller')
 router.post("/contactSeller",contactSeller)
 
+
+
+
+//seller properties
+router.get('/sellerProperties/:sellerId', async (req, res) => {
+    try {
+        const { sellerId } = req.params;
+
+        // Find properties by seller ID
+        const properties = await Property.find({ seller: sellerId });
+
+        // Check if any properties are found
+        if (properties.length === 0) {
+            return res.status(404).json({ message: 'No properties found for this seller' });
+        }
+
+        // Send the properties as the response
+        res.status(200).json(properties);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).json({ message: 'Server error' });
+    }
+});
 
 module.exports=router
